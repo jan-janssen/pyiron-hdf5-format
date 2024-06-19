403df6ca0b318d72ba7d84a69f5b615fdeaeb4a3
Jan Janssen <janssen@mpie.de>
2024-06-19 15:17:16 +0200

    Add continous integration

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyiron_io',
+    version='0.0.1',
+    url='https://github.com/pyiron-dev/pyiron-hdf5-format',
+    description='Compare how different pyiron job objects are serialised in HDF5',
+    packages=find_packages(),
+    install_requires=[
+        "pint",
+        "numpy",
+    ],
+)
